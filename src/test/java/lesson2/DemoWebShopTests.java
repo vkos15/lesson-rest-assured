d1305faa1a6abaa34a5508b03b272513a3fb83e6
Valentina <15valentinka@gmail.com>
2021-12-03 00:39:13 +0500

    added test with lof example

--- src/test/java/lesson2/DemoWebShopTests.java
+++ src/test/java/lesson2/DemoWebShopTests.java
@@ -50,6 +50,7 @@ public class DemoWebShopTests {
     void addToCartWithCookieTest() {
 
         Response response = given()
+                .log().all()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                 .body("product_attribute_72_5_18=53&product_attribute_72_6_19=54" +
                         "&product_attribute_72_3_20=57&addtocart_72.EnteredQuantity=1")
